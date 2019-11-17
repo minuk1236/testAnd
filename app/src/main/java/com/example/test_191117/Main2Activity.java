3a91b3074192f21283e4eaf20ac198d3f72044c5
minuk <40295076+minuk1236@users.noreply.github.com>
2019-11-17 22:30:56 +0900

    test

---
+++ app/src/main/java/com/example/test_191117/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.test_191117;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        int asfdasdf = 0;
+        //asfd
+    }
+}
